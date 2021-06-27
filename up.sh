9c8db2f925327ad5fcc9b7a3e7385674227aca2f
Takeda, Hiroshi <takejaponpon+git@gmail.com>
2021-06-27 13:19:26 +0000

    add option to check container instance

--- up.sh
+++ up.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-cd $(dirname $0)
-docker-compose up -d
+if [ `docker ps -f "name=inecraft-my-conf" | wc -l` -ge 2 ];
+then
+    echo "Already running. Skip" >&2
+    exit 1
+fi
 
+cd $(dirname $0)
+docker pull itzg/minecraft-bedrock-server
+docker-compose up -d
